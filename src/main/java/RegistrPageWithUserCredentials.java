5c7bef1c79757faf89ec787d0d2d21686f4a471b
SuprunO <oleksiisuprun@gmail.com>
2017-02-15 14:15:45 +0200

    Working

--- src/main/java/RegistrPageWithUserCredentials.java
+++ src/main/java/RegistrPageWithUserCredentials.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by alex on 07.02.2017.
@@ -10,6 +11,10 @@ public class RegistrPageWithUserCredentials extends PageObject {
 
     public RegistrPageWithUserCredentials(WebDriver driver){
         super(driver);
+    }
+
+    public RegistrPageWithUserCredentials(WebDriverWait driverWait){
+        super(driverWait);
     }
 
     //CSS LOCATORS
@@ -27,31 +32,31 @@ public class RegistrPageWithUserCredentials extends PageObject {
     private String CONTINUEBUTTON = ".btn.btn-primary";
 
 
-    public void verifyCredentials(String FirstName,String LastName, String EMail,String Telephone,String Address, String City, String Password) {
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(FIRSTNAME))).sendKeys(FirstName);
+    public void verifyCredentials(String FirstName,String LastName, String EMail,String Telephone,String Address, String City, String Country, String State,  String Password) {
+        driver.findElement(By.cssSelector(FIRSTNAME)).sendKeys(FirstName);
         driver.findElement(By.cssSelector(LASTNAME)).sendKeys(LastName);
         driver.findElement(By.cssSelector(EMAIL)).sendKeys(EMail);
         driver.findElement(By.cssSelector(PHONE)).sendKeys(Telephone);
         driver.findElement(By.cssSelector(ADDRESS)).sendKeys(Address);
         driver.findElement(By.cssSelector(CITY)).sendKeys(City);
-        driver.findElement(By.cssSelector(COUNTRY)).isSelected();
-        driver.findElement(By.cssSelector(STATE)).isSelected();
+        driver.findElement(By.cssSelector(COUNTRY)).sendKeys(Country);
+        driver.findElement(By.cssSelector(STATE)).sendKeys(State);
         driver.findElement(By.cssSelector(PASSWORD)).sendKeys(Password);
         driver.findElement(By.cssSelector(PASSCONFIRM)).sendKeys(Password);
         driver.findElement(By.cssSelector(RADIOBUTTON)).click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(CONTINUEBUTTON))).click();
+        driver.findElement(By.cssSelector(CONTINUEBUTTON)).click();
 
     }
 }
 
      class UserCredentialsProvider {
-        String FirstName = "Rob";
-        String Lastname = "Kawalski";
-        String EMail = "rob.kaialski@gmail.com";
-        String Telephone = "+(44)567-56-65";
-        String Address = "Brookyn st, 45";
-        String City = "New york";
-        String Country = "United States";
-        String State = "Alabama";
-        String Password = "156423";
+        public String FirstName = "Rob";
+        public String Lastname = "Kawalski";
+        public String EMail = "rob.kaialski@gmail.com";
+        public String Telephone = "+(44)567-56-65";
+        public String Address = "Brookyn st, 45";
+        public String City = "New york";
+        public String Country = "United States";
+        public String State = "Alabama";
+        public String Password = "156423";
     }
--- src/test/java/RegressionRegistrTest.java
+++ src/test/java/RegressionRegistrTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -9,16 +10,17 @@ import org.testng.annotations.Test;
 public class RegressionRegistrTest {
 
 
-    WebDriver driver=new FirefoxDriver();
-    RegistrPageWithUserCredentials registrProcess= new RegistrPageWithUserCredentials(driver);
+    WebDriver driver = new FirefoxDriver();
+    RegistrPageWithUserCredentials registrProcess = new RegistrPageWithUserCredentials(driver);
+    UserCredentialsProvider dataProvider = new UserCredentialsProvider();
 
     @BeforeTest
     public void startUp(){
         driver.get("http://kidsclotheslab.com/index.php?route=account/register");
     }
 
-
     @Test
     public void registrationTest () {
+        registrProcess.verifyCredentials(dataProvider.FirstName,dataProvider.Lastname,dataProvider.EMail,dataProvider.Telephone,dataProvider.Address,dataProvider.City,dataProvider.Country,dataProvider.State,dataProvider.Password);
     }
 }
