08aa8aa28032172f7099e06c3a1288a83d53b988
SuprunO <oleksiisuprun@gmail.com>
2017-05-05 17:23:51 +0300

    linux version

--- src/main/java/constantElements/Layout.java
+++ src/main/java/constantElements/Layout.java
@@ -11,7 +11,7 @@ import technical.BasePage;
  */
 public class Layout extends BasePage {
 
-    public Layout (WebDriver driver) {
+    public Layout(WebDriver driver) {
         super(driver);
     }
 
@@ -23,5 +23,90 @@ public class Layout extends BasePage {
         return new CartPopUp(driver);
     }
 
+    //Header
+    public WebElement getLoginLinkText() {
+        return driver.findElement(By.linkText("Login"));
+    }
 
+    public WebElement getHomeLinkText() {
+        return driver.findElement(By.linkText("Home"));
+    }
+
+    public WebElement getContactUsLinkText() {
+        return driver.findElement(By.linkText("Contact Us"));
+    }
+
+    public WebElement getCartLinkText() {
+        return driver.findElement(By.cssSelector("#cart-total"));
+    }
+
+
+    //Add categories
+    public WebElement getFirstCategoryText() {
+        return driver.findElement(By.linkText("Wedding Dresses"));
+    }
+
+
+    public WebElement getSecondCategoryText() {
+        return driver.findElement(By.linkText("Mother of the Bride"));
+    }
+
+    public WebElement getThirdCategoryText() {
+        return driver.findElement(By.linkText("Bridesmaid Dresses"));
+    }
+
+
+    public WebElement getFeaturedProductsHeaderText() {
+        return driver.findElement(By.cssSelector("#col-sm-35>h3"));
+    }
+
+
+    //footer headers
+    public WebElement getInformationHeaderText() {
+        return driver.findElement(By.cssSelector("#col-sm-80>h5"));
+    }
+
+    public WebElement getMyAccountHeaderText() {
+        return driver.findElement(By.linkText("My Account"));
+    }
+
+    public WebElement getOurOffersHeaderText() {
+        return driver.findElement(By.cssSelector("#col-sm-82>h5"));
+    }
+
+    public WebElement getCompanyInfoHeaderText() {
+        return driver.findElement(By.cssSelector("#col-sm-83>h5"));
+    }
+
+    public WebElement getContactUsHeaderTextFooter() {
+        return driver.findElement(By.linkText("Contact Us"));
+    }
+
+
+    //footer links
+
+
+    public WebElement getDeliveryTextFooter() {
+        return driver.findElement(By.linkText("Delivery Information"));
+    }
+
+    public WebElement getPrivacyPolicyTextFooter() {
+        return driver.findElement(By.linkText("Privacy Policy"));
+    }
+
+    public WebElement getTermsConditionsTextFooter() {
+        return driver.findElement(By.linkText("Terms & Conditions"));
+    }
+
+    public WebElement getMyAccountTextFooter() {
+        return driver.findElement(By.linkText("My Account"));
+    }
+
+    public WebElement getOrderHistoryTextFooter() {
+        return driver.findElement(By.linkText("Order History"));
+    }
+
+    public WebElement getSpecialsLinkText() {
+        return driver.findElement(By.linkText("Specials"));
+    }
 }
--- src/test/java/UIPageTests/UIHomePage.java
+++ src/test/java/UIPageTests/UIHomePage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import sitePages.HomePage;
@@ -18,24 +19,127 @@ import static technical.BasePage.SiteURL;
 public class UIHomePage {
 
     WebDriver driver;
-    HomePage homepage;
+    HomePage homePage;
 
     @BeforeClass
     public void startUp() {
         System.setProperty("webdriver.gecko.driver", "//home//alexei//geckodriver");
         driver = new FirefoxDriver();
         driver.get(SiteURL);
-        homepage = new HomePage(driver);
+        homePage = new HomePage(driver);
 
     }
 
     @Test
-    public void homeLinkCheck(){
-        Assert.assertEquals(homepage.get_lnk_Home().getText(),"Home");
+    public void verifyHomeLinkText(){
+        Assert.assertEquals(homePage.getLayout().getHomeLinkText().getText(),"Home","Fix Home link text to Home");
+    }
+
+    @Test
+    public void verifyContactUsText(){
+        Assert.assertEquals(homePage.getLayout().getContactUsLinkText().getText(),"Contact Us","Fix Contact US link text to Home");
+    }
+
+    @Test
+    public void verifyLoginText(){
+        Assert.assertEquals(homePage.getLayout().getLoginLinkText().getText(),"Login","Fix Login link text to Login");
+    }
+
+    @Test
+    public void verifyCartLinkText(){
+        Assert.assertEquals(homePage.getLayout().getCartLinkText().getText().trim(),"0 item(s) - $0","Fix Login link text to 0 item(s) - $0");
+    }
+
+    @Test
+    public void verifyFirstCategoryText(){
+        Assert.assertEquals(homePage.getLayout().getFirstCategoryText().getText(),"Wedding Dresses","Fix first category link text to Wedding Dresses");
+    }
+
+    @Test
+    public void verifySecondCategoryText(){
+        Assert.assertEquals(homePage.getLayout().getSecondCategoryText().getText(),"Mother of the Bride","Fix second category link text to Mother of the Bride");
+    }
+
+    @Test
+    public void verifyThirdCategoryText(){
+        Assert.assertEquals(homePage.getLayout().getThirdCategoryText().getText(),"Bridesmaid Dresses","Fix third category link text to Bridesmaid Dresses");
+    }
+
+    @Test
+    public void verifyFeaturedProductsHeaderText(){
+        Assert.assertEquals(homePage.getLayout().getFeaturedProductsHeaderText().getText(),"Featured Products","Fix header Featured Products to Featured Products");
+    }
+
+    @Test
+    public void verifyInformationHeaderTextFooter(){
+        Assert.assertEquals(homePage.getLayout().getInformationHeaderText().getText(),"INFORMATION","The Information header is written wrong, fix to Information");
+    }
+
+    @Test
+    public void verifyMyAccountHeaderTextFooter(){
+        Assert.assertEquals(homePage.getLayout().getMyAccountHeaderText().getText(),"My Account","The My Account header is written wrong, fix to My Account");
+    }
+
+    @Test
+    public void verifyOurOffersHeaderTextFooter(){
+        Assert.assertEquals(homePage.getLayout().getOurOffersHeaderText().getText(),"OUR OFFERS","The Our Offers header is written wrong, fix to Our Offers");
+    }
+
+    @Test
+    public void verifyCompanyInfoHeaderTextFooter(){
+        Assert.assertEquals(homePage.getLayout().getCompanyInfoHeaderText().getText(),"COMPANY INFO","The Company Info header is written wrong, fix to Company Info");
+    }
+
+    @Test
+    public void verifyContactUsHeaderTextFooter(){
+        Assert.assertEquals(homePage.getLayout().getContactUsHeaderTextFooter().getText(),"Contact Us","The Contact Us header is written wrong, fix to Contact Us");
+    }
+
+    @Test
+    public void verifyDeliveryLinkTextFooter(){
+        Assert.assertEquals(homePage.getLayout().getDeliveryTextFooter().getText(),"Delivery Information","The Delivery header is written wrong, fix to Delivery");
+    }
+
+    @Test
+    public void verifyPrivacyPolicyLinkTextFooter(){
+        Assert.assertEquals(homePage.getLayout().getPrivacyPolicyTextFooter().getText(),"Privacy Policy","The Privacy Policy header is written wrong, fix to Privacy Policy");
     }
 
 
+    @Test
 
+    public void verifyTermsConditionsLinkTextFooter(){
+        Assert.assertEquals(homePage.getLayout().getTermsConditionsTextFooter().getText(),"Terms & Conditions","Terms & Conditions header is written wrong, fix to Terms & Conditions");
+    }
+
+    @Test
+
+    public void verifyMyAccountLinkText(){
+        Assert.assertEquals(homePage.getLayout().getMyAccountTextFooter().getText(),"My Account","My Account link is written wrong, fix to My Account");
+    }
+
+    @Test
+
+    public void verifyOrderHistoryLinkText(){
+        Assert.assertEquals(homePage.getLayout().getOrderHistoryTextFooter().getText(),"Order History","Order History is written wrong, fix to Order History");
+    }
+
+    @Test
+
+    public void verifyTermsSpecialsLinkTextFooter(){
+        Assert.assertEquals(homePage.getLayout().getSpecialsLinkText().getText(),"Specials","Specials is written wrong, fix to Specials");
+    }
+
+
+    @AfterClass
+    public void cleanUp() {
+        try {
+            driver.close();
+            driver.quit();
+        } catch (Exception e) {
+            System.out.println("some errors occured during closing driver: \n" + e);
+        }
+    }
 
 
 }
